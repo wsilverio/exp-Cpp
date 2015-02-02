2af80d3241144e3c1fe4bc2620d0b562258ba725
wsilverio <wendeurick@gmail.com>
2015-02-02 15:46:25 -0200

    nvs arqvs add

---
+++ defaultFunc/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int soma(int a; int b; int p = 0){
+	return 15;//(p)?(a + b):(int)(a+b);
+}
+
+int main(){
+
+	soma(1,2,3);
+	return 0;
+}
---
+++ strucTeste/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+struct Teste{
+    int a, b;
+
+    int min(void) {
+        return (a<b)?a:b;
+    }
+
+    int max(void){
+        return (a>b)?a:b;
+    }
+};
+
+int main(void) {
+
+    Teste teste;
+
+    teste.a = 1;
+    teste.b = 2;
+
+    cout << "a: " << teste.a << endl;
+    cout << "b: " << teste.b << endl;
+    cout << "min(): " << teste.min() << endl;
+    cout << "max(): " << teste.max() << endl;
+
+    return 0;
+}
